09090f3c4ff4cde76d79f64a1de69640dd924b78
dhrypt <168616960+dhrypt@users.noreply.github.com>
2024-08-11 07:40:18 +0530

    Header Styling

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
@@ -7,6 +8,13 @@ const NoteCard = ({ note }) => {
 
   return (
     <div className="card" style={{ backgroundColor: colors.colorBody }}>
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+
       <div className="card-body">
         <textarea
           style={{ color: colors.colorText }}
