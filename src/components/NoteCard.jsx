fcc42fa9223adf42250448d9f8b154053c46e417
dhrypt <168616960+dhrypt@users.noreply.github.com>
2024-08-13 05:29:14 +0530

    Mouse move event

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -29,6 +29,24 @@ const NoteCard = ({ note }) => {
     document.addEventListener("mousemove", mouseMove);
   };
 
+  const mouseMove = (e) => {
+    //1 - Calculate move direction
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    //2 - Update start position for next move.
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    //3 - Update card top and left position.
+    setPositon({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    });
+  };
+
   return (
     <div
       ref={cardRef}
@@ -40,6 +58,7 @@ const NoteCard = ({ note }) => {
       }}
     >
       <div
+        onMouseDown={mouseDown}
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
       >
