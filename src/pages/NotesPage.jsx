6ed8c729e610a00e0565c87300ddb48f91fbfc73
dhrypt <168616960+dhrypt@users.noreply.github.com>
2024-08-13 22:20:54 +0530

    Database config

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-import { fakeData as notes } from "../assets/Fakedata";
+import { useState, useEffect } from "react";
+// import { fakeData as notes } from "../assets/Fakedata";
+import { db } from "../appwrite/databases";
 import NoteCard from "../components/NoteCard";
 
 const NotesPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const response = await db.notes.list();
+    setNotes(response.documents);
+  };
+
   return (
     <div>
       {notes.map((note) => (
