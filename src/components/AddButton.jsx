dd4efbf5ef6cd2fe00983239c98e66beea075dd3
dhrypt <168616960+dhrypt@users.noreply.github.com>
2024-08-19 00:00:49 +0530

    addNote method

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,8 +1,24 @@
+import { useRef } from "react";
 import Plus from "../icons/Plus";
+import { db } from "../appwrite/databases";
 
 const AddButton = () => {
+  const startingPos = useRef(10);
+
+  const addNote = async () => {
+    const payload = {
+      position: JSON.stringify({
+        x: startingPos.current,
+        y: startingPos.current,
+      }),
+      colors: JSON.stringify(colors[0]),
+    };
+
+    const response = await db.notes.create(payload);
+  };
+
   return (
-    <div id="add-btn">
+    <div id="add-btn" onClick={addNote}>
       <Plus />
     </div>
   );
